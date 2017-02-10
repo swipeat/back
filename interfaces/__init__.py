f0c5157cc99f50897c1066fcfaaa3d9657e6ca41
Nils Schaetti <n.schaetti@gmail.com>
2017-02-10 23:52:41 +0100

    Add secret key

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -6,9 +6,11 @@ from flask import Flask
 # Flask app settings
 app = Flask(__name__)
 app.debug = True
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 # Import interfaces
 import interfaces.list
 import interfaces.recipe
 import interfaces.login
 import interfaces.meals
+import interfaces.profile
