b0c040bebd47c7b240ef0da395b9feae41d74cf2
Loïc Jaouen <loic.jaouen@unil.ch>
2017-02-11 12:08:49 +0100

    merging

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -3,12 +3,17 @@
 # Import
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from query import Dishes
+from query import Ingredients
 
 # Flask app settings
 app = Flask(__name__)
 CORS(app)
 app.debug = True
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+Dishes.create()
+Ingredients.create()
 
 # Import interfaces
 import interfaces.list
--- interfaces/dishes.py
+++ interfaces/dishes.py
@@ -15,24 +15,24 @@ def get_dishes():
     Requires GET request with fields::
 
         "constraints" : the constraints (e.g., vegan, italian, etc.) separated by commas
-        "nummeals" :  number of meals to return
+        "numdishes" :  number of dishes to return
 
-    :return: JSON of meals. For each "meal_id" it has "title" and "image_url".
+    :return: JSON of dishes. For each "dish_id" it has "title" and "image_url".
     """
 
     # Get the constraints from the GET request
     constraints = request.args.get("constraints").split(",")
-    # Get the number of meals
-    number_of_meals = request.args.get("nummeals")
+    # Get the number of dishes
+    number_of_dishes = request.args.get("numdishes")
 
-    # create meal object for the specific API querying
+    # create dish object for the specific API querying
     foodobject = Food2forkDish(constraints)
-    # static wrapper for meal objects
-    meals_selector = Dishes.instance()
-    meals_selector.set_query_object(foodobject)
-    # get the meals given the number
-    returned_meals = meals_selector.get_dishes(number_of_meals)
-    if returned_meals:
-        return json.dumps({"response": 0, "message": "OK", "results": returned_meals})
+    # static wrapper for dish objects
+    dishes_selector = Dishes.instance()
+    dishes_selector.set_query_object(foodobject)
+    # get the dishes given the number
+    returned_dishes = dishes_selector.get_dishes(number_of_dishes)
+    if returned_dishes:
+        return json.dumps({"response": 0, "message": "OK", "results": returned_dishes})
     else:
-        return json.dumps({"response": -1, "message": "Error: no meals were found"})
+        return json.dumps({"response": -1, "message": "Error: no dishes were found"})
