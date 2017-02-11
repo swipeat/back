f29972f21d63dbf03ebfb0d4dc6995d851cfaed5
Nils Schaetti <n.schaetti@gmail.com>
2017-02-11 02:31:42 +0100

    List support

---
+++ db/list.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# @author: Nils Schaetti <n.schaetti@gmail.com>
+# @date: Fri 11 Feb
+
+import sqlite3
+
+# Get the complete list of the ingredient
+def get_list(username):
+    """ Get the complete list of the ingredient """
+
+    # Connection and cursor
+    conn = sqlite3.connect('swipeat.db')
+    c = conn.cursor()
+
+    # Search for the ingredient
+    c.execute('''SELECT * FROM swipeat_lists WHERE username like \'''' + username + "\'")
+    return c.fetchall()
+
+# Remove ingredient from the list
+def remove_ingredient(username, ingredient):
+    """ Remove ingredient from the list """
+
+    # Connection and cursor
+    conn = sqlite3.connect('swipeat.db')
+    c = conn.cursor()
+
+    # Remove
+    c.execute('''DELETE FROM swipeat_lists WHERE username like \'''' + username + '''\' AND ingredient like \'''' + ingredient + "\'")
+    conn.commit()
+
+# Remove all ingredients from the list
+def purge_list(username):
+    """ Remove all ingredients from the list """
+
+    # Connection and cursor
+    conn = sqlite3.connect('swipeat.db')
+    c = conn.cursor()
+
+    # Remove all
+    c.execute('''DELETE FROM swipeat_lists WHERE username LIKE \'''' + username + "\'")
+    conn.commit()
+
+# Is an ingredient in the list
+def is_in_list(username, ingredient):
+    """
+    Is an ingredient in the user's list?
+    :param username: User's name
+    :param ingredient: Ingredient to test
+    :return: Yes or no
+    """
+
+    # Connection and cursor
+    conn = sqlite3.connect('swipeat.db')
+    c = conn.cursor()
+
+    # Search for the ingredient
+    c.execute('''SELECT * FROM swipeat_lists WHERE username like \'''' + username + "\'")
+    rows = c.fetchall()
+
+    # Check results
+    if len(rows) > 0:
+        return True
+    else:
+        return False
+
+
+# Add an ingredient to the list
+def add_ingredient(username, ingredient):
+    """
+    An an ingredient to the user's list
+    :param username: User's name
+    :param ingredient: Ingredient to add
+    :return: Nothing
+    """
+
+    # If not existing
+    if not is_in_list(username, ingredient):
+        # Connection and cursor
+        conn = sqlite3.connect('swipeat.db')
+        c = conn.cursor()
+
+        # Add the ingredient
+        c.execute('''INSERT INTO swipeat_lists(username, ingredient) VALUES(:username,:ingredient)''', {'username': username, 'ingredient': ingredient})
+        conn.commit()
--- interfaces/list.py
+++ interfaces/list.py
@@ -1,13 +1,153 @@
-#
-# list.py : Interfaces to access list.
-#
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# @author: Nils Schaetti <n.schaetti@gmail.com>
+# @date: Fri 10 Feb
 
 from interfaces import app
+from db import list
+from db import user
 import json
+from flask import request
+from query import Ingredients
+from query import Food2forkIngredients
+from flask import session
 
 # /list/get : Get the current list
 @app.route("/list/get")
-def getList():
-    """ Interface /list/get
-        Get current lists """
-    return json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True)
+def get_user_list():
+    """
+    Get the complete list of the user with all the current ingredients
+    :return: A JSON string with the current list of ingredients.
+    """
+
+    # Session informations
+    username = session["username"]
+    password = session["password"]
+
+    # Check login
+    cond, msg = user.check_login(username, password)
+    if not cond:
+        return json.dumps({"response": -1, "message": msg})
+
+    # Get data
+    rows = list.get_list(username)
+
+    # For each rows
+    results = []
+    for row in rows:
+        results += [row[1]]
+
+    return json.dumps({"response": 0, "message": "Ok", "count": len(results), "results": results})
+
+# /list/remove_ingredient : remove an ingredient from the list
+@app.route("/list/remove_ingredient")
+def remove_ingredient():
+    """
+    Remove an ingredient from the list
+    :return:
+    """
+
+    # Session informations
+    username = session["username"]
+    password = session["password"]
+
+    # Check login
+    cond, msg = user.check_login(username, password)
+    if not cond:
+        return json.dumps({"response": -1, "message": msg})
+
+    # Parameters
+    ingredient = request.form["ingredient"]
+
+    # Remove
+    list.remove_ingredient(username, ingredient)
+
+    return json.dumps({"response": 0, "message": "Ok"})
+
+
+# /list/purge : Purge the list of ingredient
+@app.route("/list/purge")
+def purge_user_list():
+    """
+    Purge the user's list of ingredients
+    :return: Confirmation
+    """
+
+    # Session informations
+    username = session["username"]
+    password = session["password"]
+
+    # Check login
+    cond, msg = user.check_login(username, password)
+    if not cond:
+        return json.dumps({"response": -1, "message": msg})
+
+    # Remove
+    list.purge_list(username)
+
+    return json.dumps({"response": 0, "message": "OK"})
+
+# /list/add_ingredient : Add an ingredient to the list
+@app.route("/list/add_ingredient", methods=["POST"])
+def add_ingredient():
+    """
+    Add an ingredient to the user's list
+    :return: A JSON string confirmation for the client
+    """
+
+    # Session informations
+    username = session["username"]
+    password = session["password"]
+
+    # Check login
+    cond, msg = user.check_login(username, password)
+    if not cond:
+        return json.dumps({"response": -1, "message": msg})
+
+    # Parameters
+    ingredient = request.form["ingredient"]
+
+    # Add the ingredient
+    list.add_ingredient(username, ingredient)
+
+    return json.dumps({"response": 0, "message": "OK"})
+
+# /list/add_recipe : Add all ingredients of a recipe to the user's list
+@app.route("/list/add_recipe", methods=["POST"])
+def add_recipe():
+    """
+    Add all ingredients of a recipe to the user's list.
+    :return: A JSON list of ingredients added.
+    """
+
+    # Session informations
+    username = session["username"]
+    password = session["password"]
+
+    # Check login
+    cond, msg = user.check_login(username, password)
+    if not cond:
+        return json.dumps({"response": -1, "message": msg})
+
+    # Get the the meal Ids that were selected by user from the POST request
+    meal_ids = request.form["mealsids"].split(",")
+
+    # Create meal object for the specific API querying
+    ingredientobject = Food2forkIngredients(meal_ids)
+
+    # Static wrapper for meal objects
+    ingredient_selector = Ingredients.instance()
+    ingredient_selector.set_query_object(ingredientobject)
+
+    # Get the meals given the number
+    returned_ingredients = ingredient_selector.get_ingredients()
+
+    # If ok
+    if returned_ingredients:
+        print returned_ingredients
+        for ingredient in returned_ingredients["ingredients"]:
+            # Add the ingredient
+            list.add_ingredient(username, ingredient)
+        return json.dumps({"response": 0, "message": "Ok", "results": returned_ingredients["ingredients"]})
+    else:
+        return json.dumps({"response": -1, "message": "Error: no meals were found"})
