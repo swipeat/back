7e8791543d605589238eb6d8e3b5d982e5db2b51
Nils Schaetti <n.schaetti@gmail.com>
2017-02-11 02:29:55 +0100

    New tables

--- create_db.py
+++ create_db.py
@@ -10,3 +10,10 @@ c = conn.cursor()
 c.execute("CREATE TABLE swipeat_accounts (username text, password text)")
 conn.commit()
 
+# Create swipeat_profiles table
+c.execute("CREATE TABLE swipeat_profiles (username text, ingredient text, note integer)")
+conn.commit()
+
+# Create swipeat_lists table
+c.execute("CREATE TABLE swipeat_lists (username text, ingredient text)")
+conn.commit()
