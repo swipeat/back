6a8a921892cfa73590e6ba1fd1fdc83de93e6f23
Sébastien Vermeille <sebastien.vermeille@gmail.com>
2017-02-11 02:20:34 +0100

    CORS FIX

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -2,9 +2,11 @@
 
 # Import
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 # Flask app settings
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
